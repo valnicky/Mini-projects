e8a6a488fe0639b1741138c6791408cd2407d1ac
Valeria <valery0007@gmail.com>
2020-12-09 21:30:10 +0100

    rock paper scissors js

--- static/js/script.js
+++ static/js/script.js
@@ -18,7 +18,7 @@ function ageInDays() {
 function reset() {
     document.getElementById('ageInDays').remove();
 }
-
+/*challenge 2 */
 function generate() {
     let foto = document.createElement('div');
     let image = document.createElement('img');
@@ -29,4 +29,47 @@ function generate() {
 
     result.appendChild(foto);
     foto.appendChild(image);
+}
+
+/*challenge 3*/
+
+/*1. Paper beats Rocks
+2. Rock beats Scissors
+3. Scissors beats Paper */
+
+let arr = ['Rock', 'Paper', 'Scissors'];
+
+function choose(yourChoice) {
+
+    let humanChoice, botChoice;
+    humanChoice = yourChoice.id;
+    botChoice = numberToChoice(randToRpsInt());
+    console.log(botChoice);
+    let results = decideWinner(humanChoice, botChoice); // [1, 0] human wins
+
+    let message = finalMessage(results); //{'message': 'You won!', 'color': 'green'}
+
+    rpsFrontEnd(humanChoice.id, botChoice, message);
+}
+
+function randToRpsInt() {
+    return Math.floor(Math.random() * 3);
+}
+
+function numberToChoice(number) {
+    return arr[number];
+}
+
+function decideWinner(yourChoice, computerChoice) {
+    let rpsDatabase = {
+        'rock': { 'scissors': 1, 'rock': 0.5, 'paper': 0 },
+        'paper': { 'rock': 1, 'paper': 0.5, 'scissors': 0 },
+        'scissors': { 'paper': 1, 'scissors': 0.5, 'rock': 0 }
+    }
+
+    let yourScore = rpsDatabase[yourChoice][computerChoice];
+
+    let computerScore = rpsDatabase[computerChoice][yourChoice];
+
+    return [yourScore, computerScore];
 }
